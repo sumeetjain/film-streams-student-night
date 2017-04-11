dff20f5a8279c173ca9fa13de89e50346093d096
Michael Sefranek <msefran@gmail.com>
2017-04-11 14:07:37 -0500

    added resources users

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,14 @@ Rails.application.routes.draw do
 
   resources :students
   resources :events
+  resources :movies
+  resources :attendances
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'attendances#new'
-
-   resources :movies
-   resources :attendances
+   root 'users#new'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
