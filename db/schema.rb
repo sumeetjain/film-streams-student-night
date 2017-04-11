c491d1a8376defd8002eb413ce21a8ad6bef4c83
Michael Sefranek <msefran@gmail.com>
2017-04-11 13:43:29 -0500

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-
-ActiveRecord::Schema.define(version: 20170411163254) do
-
+ActiveRecord::Schema.define(version: 20170411184126) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "attendances", force: :cascade do |t|
+    t.integer  "student_id"
+    t.integer  "movie_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "events", force: :cascade do |t|
     t.date     "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "movies", force: :cascade do |t|
+    t.string   "title"
+    t.time     "time"
+    t.integer  "event_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -36,19 +48,11 @@ ActiveRecord::Schema.define(version: 20170411163254) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "attendances", force: :cascade do |t|
-    t.integer  "student_id"
-    t.integer  "movie_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "movies", force: :cascade do |t|
-    t.string   "title"
-    t.time     "time"
-    t.integer  "event_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
