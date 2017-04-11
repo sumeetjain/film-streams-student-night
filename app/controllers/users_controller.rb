d76a4c987ff98ea9db027173f3d13c443ab39276
Michael Sefranek <msefran@gmail.com>
2017-04-11 16:23:03 -0500

    user class not accepting name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,15 @@
 class UsersController < ApplicationController
 	def create
+		debugger
 		@current_user = User.new(user_params)
+	    if @current_user.save && @current_user.authenticate(params[:user][:password])
+    	session[:user_id] = @current_user.id
+    	flash[:success] = "Welcome to Film Streams Student Night!"
+    	redirect_to events_index
+	  else
+	  	@current_user
+	    	render 'new'
+	  end
 	end
 
 	def user_params
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
-	  attr_accessor :username, :password, :password_confirmation
+  # attr_accessor :username, :password, :password_confirmation
+
+	# validates :username, presence: true
+	# has_secure_password
+	# validates :password, presence: true, length: { minimum: 6 }
 end
