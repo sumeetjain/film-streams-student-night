875f5880593239b1d2c40dfd0a4b069f21d5b50d
Michael Sefranek <msefran@gmail.com>
2017-04-11 15:32:45 -0500

    login going to correct page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,10 @@
 class UsersController < ApplicationController
+	def create
+		@current_user = User.new(user_params)
+	end
+
+	def user_params
+		params.require(:user).permit(:username, :password_digest,
+											:password_confirmation)
+	end
 end
