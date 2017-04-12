56fa03a816a03142905c39fffb5b70831e6572cc
Michael Sefranek <msefran@gmail.com>
2017-04-11 21:08:48 -0500

    added tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
 
 	validates :username, presence: true
 	validates :password_digest, presence: true, length: { minimum: 6 }
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence: true, length: { maximum: 255},
+										format: { with: VALID_EMAIL_REGEX }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,10 +1,10 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-	def setup
-		@user = User.new(username: "Example User", password_digest: "moocows")
-	end
 
+	def setup
+		@user = User.new(username: "Example User", password_digest: "moocows", email: "moogle@doogle.com")
+	end
 
 	test "should be valid" do
 		assert @user.valid?
@@ -18,6 +18,24 @@ class UserTest < ActiveSupport::TestCase
 	test "password should be present" do
 		@user.password_digest = "    "
 		assert_not @user.valid?
+	end
+
+	test "email validation should accept valid addresses" do
+		valid_addresses = %w[user@example.com User@foo.COM A_US-ER@foo.bar.org
+												 first.last@foo.jp alice+bob@baz.cn]
+		valid_addresses.each do |valid_address|
+			@user.email = valid_address
+			assert @user.valid?, "#{valid_address.inspect} should be valid"
+		end
+	end
+
+	test "email validation should not accept invalid addresses" do
+		invalid_addresses = %w[lskolksndg bork@ derp.corp moo,moo,moo 
+													user.name.@example foo@bar_baz.com foo@bar+dong.mom]
+		invalid_addresses.each do |invalid_address|
+			@user.email = invalid_address
+			assert_not @user.valid?, "#{invalid_address.inspect} should not be valid"
+		end
 	end
 end
 
