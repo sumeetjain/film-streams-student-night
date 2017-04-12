eb21ee9f5526d80d6ac43727492e67da06c43fc7
Michael Sefranek <msefran@gmail.com>
2017-04-12 11:34:31 -0500

    tying routes for buttons

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,5 +1,9 @@
 class AttendancesController < ApplicationController
 
+	def index
+
+	end
+
 	def show
 		@event = Event.find(params[:id])
 		@student = Student.find_by(email: session[:email]) ? Student.find_by(email: session[:email]) : Student.new
