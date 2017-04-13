7166a924244fda3744b84ce363bdc7a7fd2d82d7
Michael Sefranek <msefran@gmail.com>
2017-04-12 22:16:25 -0500

    password nonempty test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -24,9 +24,11 @@ RSpec.describe User, type: :model do
   end
 
   describe '#allows valid email' do
+  	# Setup
   	it 'is valid if conforms to regex' do
 			valid_addresses = %w[user@example.com User@foo.COM A_US-ER@foo.bar.org
 													 first.last@foo.jp alice+bob@baz.cn]
+		# Execute / Validation
 			valid_addresses.each do |valid_address|
 				$user.email = valid_address
 				expect($user.valid?).to equal (true)
@@ -36,8 +38,10 @@ RSpec.describe User, type: :model do
 
   describe '#does not allow invalid email' do
   	it 'is not valid with improper regex' do
+  		# Setup
 			invalid_addresses = %w[lskolksndg bork@ derp.corp moo,moo,moo 
 		 												user.name.@example foo@bar_baz.com foo@bar+dong.mom]
+		 	# Execute / Validation
 			invalid_addresses.each do |invalid_address|
 				$user.email = invalid_address
 				expect($user.valid?).to equal (false)
@@ -47,13 +51,25 @@ RSpec.describe User, type: :model do
 
   describe '#email should be unique' do
   	it 'is invalid if duplicate' do
+  		# Setup
   		dup_user = $user.dup
   		dup_user.email = $user.email.upcase
   		$user.save
+  		# Execute / Validation
   		expect(dup_user.valid?).to equal (false)
   	end
   end
-  
+
+  describe '#password should be nonempty' do
+  	# Setup
+  	it 'is invalid if password empty' do
+  		user = User.new(password: "  ")
+  		expect(user.valid?).to equal (false)
+  	end
+  end
+
+
+
 	# test "password should be nonempty" do
 	# 	@user.password_digest = @user.password_confirmation = " "
 	# 	assert_not @user.valid?
@@ -62,7 +78,6 @@ RSpec.describe User, type: :model do
 	# test "password should have min length" do
 	# 	@user.password_digest = @user.password_confirmation = "a"
 	# 	assert_not @user.valid?
-	# end 
-
+	# end
 
 end
