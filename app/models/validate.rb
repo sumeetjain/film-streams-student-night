b1fe49b5fb8b1b4912989d684c57a44e72b570b6
Allen Wipf <allenwipf@msn.com>
2017-04-13 09:39:41 -0500

    fixed-routing

--- app/models/validate.rb
+++ app/models/validate.rb
@@ -9,12 +9,14 @@ class Validate
 		is_a_valid_email? and emails_match?
 	end
 
-	def is_a_valid_email?
-  	(@email =~ /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i)
-	end
+	private 
 
-	def emails_match?
-		@email == @email_confirmation
-	end
+		def is_a_valid_email?
+	  	(@email =~ /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i)
+		end
+
+		def emails_match?
+			@email == @email_confirmation
+		end
 
 end
