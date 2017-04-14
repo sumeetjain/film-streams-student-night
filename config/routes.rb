fa24ea76c8cdf1238e9888ed5d4f36d519028dc4
Michael Sefranek <msefran@gmail.com>
2017-04-14 12:02:30 -0500

    fixed buttons on events index

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   # resources :movies
   resources :users
   resources :login
+  resources :stats, only: [:index]
   get  '/logout'                  => 'login#destroy'
 
   # ---------------------------------------------------------------------------
