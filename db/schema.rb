109cd6b46ab151e8deac5a9d7019ed58d3292af7
Sumeet Jain <sumeet@sumeetjain.com>
2017-04-19 10:09:12 -0500

    Import schools, updated form

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170419143555) do
+ActiveRecord::Schema.define(version: 20170419150510) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,7 +48,7 @@ ActiveRecord::Schema.define(version: 20170419143555) do
   create_table "students", force: :cascade do |t|
     t.string   "email"
     t.string   "name"
-    t.integer  "school"
+    t.integer  "school_id"
     t.integer  "year"
     t.integer  "zip"
     t.integer  "referral"
