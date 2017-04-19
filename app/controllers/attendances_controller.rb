8c06bf9e684fa6373fa14bac698580001a20402a
Sumeet Jain <sumeet@sumeetjain.com>
2017-04-19 12:14:07 -0500

    Styling merging

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -5,22 +5,23 @@ class AttendancesController < ApplicationController
 	def new
 		# Use this to show the movies for this event.
 		@event = Event.find(params[:event_id])
-
-		# We need to pass this along.
-		@student_id = flash[:student_id]
+		@attendance = Attendance.new(event_id: @event.id, student_id: flash[:student_id])
+		
 	end
 
 	# Save this student's attendance, and redirect to the event's
 	# main page for the next student to check in.
 	def create
-		Attendance.create(attendance_params)
-		redirect_to new_event_checkin_path(params[:event_id])
+		attendance = Attendance.create(attendance_params)
+		msg = "Thanks, #{attendance.student.name}. You're checked in."
+
+		redirect_to new_event_checkin_path(params[:event_id]), notice: msg
 	end
 
 	private
 
 	def attendance_params
-		params.permit(:movie_id, :student_id, :event_id)
+		params.require(:attendance).permit(:movie_id, :student_id, :event_id)
 	end
 
 end
