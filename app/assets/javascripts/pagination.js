32417620978bda2b6e26d4a3cb6b484f95571a60
Michael Sefranek <msefran@gmail.com>
2017-04-24 09:55:13 -0500

    added comments

--- app/assets/javascripts/pagination.js
+++ app/assets/javascripts/pagination.js
@@ -6,17 +6,19 @@ window.addEventListener('load', function(){
 
 function paginate_tables(table_pagers){
 	for (var i = 0; i <= table_pagers.length-1; i++) {
-		make_nums(table_pagers[i]);
+		make_nums(table_pagers[i],1,10);
 	}
 };
 
-function make_nums(table_sibling){
+function make_nums(table_sibling,low,up){
+	//set up based on nums here if nums < up
 	table = table_sibling.previousElementSibling.children[1];
 	nums = table.childElementCount / 10;
-	for (var i = 0; i <= nums-1; i++){
+
+	for (var i = low; i <= up; i++){
 	  var li = document.createElement('li');
 	  var a = document.createElement('a');
-	  a.textContent = i+1;
+	  a.textContent = i;
 	  a.href = "\'#\'"
 	  li.appendChild(a);
 	  table_sibling.appendChild(li);		
@@ -28,3 +30,5 @@ function make_nums(table_sibling){
 // if 2 is clicked show 10-20 and set 2 to active. 3 => 20-30 etc
 // if 1 is active show 1-10 pagers
 // if 6 is active show 2 - 11, 7 => 3 - 12 etc..
+// if numbers higher than 5 or less than 5 minus total are selected, this
+// will call make nums again.
