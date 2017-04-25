ea5b21b41fd3480e3d7e8ca09b72beed60807a25
Michael Sefranek <msefran@gmail.com>
2017-04-24 21:42:49 -0500

    refactored min max into min

--- app/assets/javascripts/pagination.js
+++ app/assets/javascripts/pagination.js
@@ -2,20 +2,17 @@ window.addEventListener('load', function(){
 	var page_parents = document.getElementsByClassName("pagination");
 	var table_bodies = document.getElementsByClassName("paginate_body");
 	// for all trs, make that / 10 tabs
-	generate_page_tabs(page_parents);
-	// find the tabs
-	var tabs = document.getElementsByClassName("t_row");
-	debugger;
+	generate_page_tabs(page_parents,1,10);
 	// hide all but first 10 trs for tables
-	show_ten_table_rows(table_bodies, 0, 9);
+	show_ten_table_rows(table_bodies, 0);
 });
 
-function show_ten_table_rows(table_bodies,min,max){
+function show_ten_table_rows(table_bodies,min){
 	for (var i = 0; i <= table_bodies.length - 1; i++) {
 		rows = table_bodies[i].getElementsByTagName("tr");
 
 		for (var j = 0; j < rows.length; j++) {
-			if (j >= min && j <= max){
+			if (j >= min && j <= min+9){
 				rows[j].style.display = "table-row";
 			} else {
 				rows[j].style.display = "none";
@@ -24,12 +21,20 @@ function show_ten_table_rows(table_bodies,min,max){
 	}
 };
 
-function generate_page_tabs(page_parents){
+// For each table parent, generates tabs
+//
+// page_parents - initially a list of empty <ul>
+function generate_page_tabs(page_parents,min,max){
 	for (var i = 0; i <= page_parents.length-1; i++) {
-		make_nums(page_parents[i],1,10);
+		make_nums(page_parents[i],min,max);
 	}
 };
 
+// Creates <li> for each 10 <tr>s in <tbody>
+//
+// Inserts numbers as <a> correspondingly
+//
+// 
 function make_nums(table_sibling,min,max){
 	//set max based on nums here if nums < max
 	table = table_sibling.previousElementSibling.children[1];
@@ -37,15 +42,26 @@ function make_nums(table_sibling,min,max){
 
 	for (var i = min; i <= max; i++){
 	  var li = document.createElement('li');
+	  li.className += "jtab";
 	  var a = document.createElement('a');
 	  a.textContent = i;
 	  a.href = "\'#\'"
 	  li.appendChild(a);
 	  table_sibling.appendChild(li);	
 	}
-};
 
-// add event listeners on pagers.
+	// listen to the current tabs, for each tab
+	var tabs = document.getElementsByClassName("jtab");
+
+  for (var i = 0; i < tabs.length; i++){
+    	tabs[i].addEventListener("click", function(){
+				num = parseInt(this.innerText);
+				show_ten_table_rows(table_bodies,num);
+				generate_page_tabs(page_parents,1,10);
+    	});
+  }
+}; 
+
 // if 2 is clicked show 10-20 and set 2 to active. 3 => 20-30 etc
 // if 1 is active show 1-10 pagers
 // if 6 is active show 2 - 11, 7 => 3 - 12 etc..
