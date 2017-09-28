de9a521a2c58d6b264a36372abbfaeb7fa11afda
Sumeet Jain <sumeet@sumeetjain.com>
2017-09-28 14:42:21 -0500

    Get top ten schools for dropdown. Needs refactoring (badly)

--- Gemfile
+++ Gemfile
@@ -39,6 +39,8 @@ group :development, :test do
 end
 
 group :development do
+  gem "pry"
+
   # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 2.0'
 
--- app/models/school.rb
+++ app/models/school.rb
@@ -1,3 +1,19 @@
 class School < ActiveRecord::Base
   has_many :students
+
+  def self.top_ten
+    schools_hash = {}
+
+    School.all.each do |school|
+      total_attendances = 0
+
+      school.students.each do |student|
+        total_attendances += student.attendances.count
+      end
+
+      schools_hash[school] = total_attendances
+    end
+
+    result = schools_hash.sort_by {|school, attendances| attendances}.reverse.to_h.keys[0..9]
+  end
 end
