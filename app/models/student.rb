1e5e81c9b1eb82071969a4ebd73892299af34195
Kamin Day <kaminday@gmail.com>
2017-09-29 11:52:23 -0500

    moved referral enum from students to referrals model, renamed to referral_type

--- app/models/student.rb
+++ app/models/student.rb
@@ -66,15 +66,5 @@ class Student < ActiveRecord::Base
 		'other'										 => 999
 	}
 
-	  enum referral: {
-		'From a friend'             => 0,
-		'From my teacher'           => 1,
-		"Film Stream's enewsletter" => 2,
-		'Internet Search'						=> 3,
-		'Social media'              => 4,
-		"Film Stream's website"     => 5,
-		'other source'							=> 999
-
-	}
 
 end
