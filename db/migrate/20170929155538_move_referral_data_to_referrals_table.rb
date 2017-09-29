5cb0319b3f500b08253fcabde0170b79ce25381b
Kamin Day <kaminday@gmail.com>
2017-09-29 11:01:33 -0500

    added code to move_referral_data_to_referrals_table migration file

--- db/migrate/20170929155538_move_referral_data_to_referrals_table.rb
+++ db/migrate/20170929155538_move_referral_data_to_referrals_table.rb
@@ -1,4 +1,12 @@
+# Takes the student id and referral from each student and migrates their values to the referrals table
+
 class MoveReferralDataToReferralsTable < ActiveRecord::Migration
-  def change
-  end
+    def change
+   		Student.select('id, referral') do |student|
+			student.referrals.create(
+				:student_id => student.id,
+				:referral_type => student.referral
+		    )
+  		end
+    end
 end
