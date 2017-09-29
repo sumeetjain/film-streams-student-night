fab41ebf8b956808144aac84cb53eb3b2f4808f1
John T Cronin <jt.cronin@outlook.com>
2017-09-29 14:07:28 -0500

    all students show up with an edit button attached in a table

--- app/controllers/edit_students_controller.rb
+++ app/controllers/edit_students_controller.rb
@@ -4,4 +4,14 @@ def index
 	@student = Student.all
 end
 
+def update
+		@student = Student.find(params[:id])
+		if @event.update_attributes(event_params)
+			flash[:notice] = "Student updated!"
+		else
+			flash[:alert] = "Invalid Student Info"
+		end
+		redirect_to edit_students_path(@student.id)
+	end
+
 end
