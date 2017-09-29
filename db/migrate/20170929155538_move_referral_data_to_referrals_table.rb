981d08a7ce3ed3fc62917e92b6cfd0e1883c149b
Kamin Day <kaminday@gmail.com>
2017-09-29 11:21:21 -0500

    updated move_referral_data_to_referrals_table to a working state

--- db/migrate/20170929155538_move_referral_data_to_referrals_table.rb
+++ db/migrate/20170929155538_move_referral_data_to_referrals_table.rb
@@ -1,12 +1,16 @@
 # Takes the student id and referral from each student and migrates their values to the referrals table
 
 class MoveReferralDataToReferralsTable < ActiveRecord::Migration
-    def change
-   		Student.select('id, referral') do |student|
-			student.referrals.create(
+    def up
+   		Student.all.each do |student|
+			Referral.create!(
 				:student_id => student.id,
-				:referral_type => student.referral
+				:referral_type => Student.referrals[student.referral]
 		    )
   		end
+    end
+
+    def down
+    	Referral.destroy_all
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170419150510) do
+ActiveRecord::Schema.define(version: 20170929155538) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,13 @@ ActiveRecord::Schema.define(version: 20170419150510) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "referrals", force: :cascade do |t|
+    t.integer  "referral_type"
+    t.integer  "student_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
   create_table "schools", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
@@ -64,8 +71,4 @@ ActiveRecord::Schema.define(version: 20170419150510) do
     t.string   "email"
   end
 
-  create "referrals", force: :cascade do |t|
-    t.integer "referral_type"
-    t.integer "student_id"
-  end
 end
