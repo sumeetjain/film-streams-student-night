56bf1b520cc31e6ef4f36ab0ca1c1ce1d2def6c2
Sumeet Jain <sumeet@sumeetjain.com>
2017-10-02 13:48:06 -0500

    Fix indentation

--- app/controllers/edit_students_controller.rb
+++ app/controllers/edit_students_controller.rb
@@ -1,22 +1,24 @@
 class EditStudentsController < ApplicationController
 
-def index
-	@student = Student.order('name ASC')
-end
+	def index
+		@student = Student.order('name ASC')
+	end
 
-def edit
-	# @student1 = (student id FROM Student)
-	# @student2 = (student id FROM Student)
-	# @studentsMerge = [@student1, @student2]
-end
-def update
+	def edit
+		# @student1 = (student id FROM Student)
+		# @student2 = (student id FROM Student)
+		# @studentsMerge = [@student1, @student2]
+	end
+
+	def update
 		@student = Student.find(params[:id])
+
 		if @event.update_attributes(event_params)
 			flash[:notice] = "Student updated!"
 		else
 			flash[:alert] = "Invalid Student Info"
 		end
+
 		redirect_to edit_students_path(@student.id)
 	end
-
 end
