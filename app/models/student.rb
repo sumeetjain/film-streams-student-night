1036a68927cb4631d8ad956826e9ad1647bca628
Kamin Day <kaminday@gmail.com>
2017-10-05 16:09:14 -0500

    fixed indentation in students model

--- app/models/student.rb
+++ app/models/student.rb
@@ -14,45 +14,44 @@ class Student < ActiveRecord::Base
 	validates :email, presence: true,  uniqueness: { case_sensitive: false },
 					  format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
 
-  def update_referrals(new_referrals)
-    # Remove all this student's referrals from the DB.
-    self.referrals.destroy_all
+  	def update_referrals(new_referrals)
+  	    # Remove all this student's referrals from the DB.
+   	    self.referrals.destroy_all
 
-    # Add new_referrals to this student.
-    new_referrals.each do |r|
-      Referral.create!(student_id: self.id, referral_type: r.to_i)
+  	     # Add new_referrals to this student.
+        new_referrals.each do |r|
+      		Referral.create!(student_id: self.id, referral_type: r.to_i)
+  	    end
     end
-  end
-
 
 	def validates_email
 		if Student.where(email: email).exists?
     	return true
-  	end
-  end
+  		end
+    end
 
-  def add_to_mailchimp
-  	if newsletter == "1"
-	  	begin
-	  		Gibbon::Request.lists(ENV["MAILCHIMP_LIST_ID"]).members.create(body: {email_address: email, status: "subscribed", merge_fields: {FNAME: first_name, LNAME: last_name}})
-	  	rescue Gibbon::MailChimpError => e
-	  		# Deal with the error in any way? Undecided.
-	  	end
-	  end
-  end
+    def add_to_mailchimp
+    	if newsletter == "1"
+		  	begin
+		  		Gibbon::Request.lists(ENV["MAILCHIMP_LIST_ID"]).members.create(body: {email_address: email, status: "subscribed", merge_fields: {FNAME: first_name, LNAME: last_name}})
+		  	rescue Gibbon::MailChimpError => e
+		  		# Deal with the error in any way? Undecided.
+		  	end
+	    end
+    end
 
-  def first_name
-  	name.split(" ").first
-  end
+    def first_name
+    	name.split(" ").first
+    end
 
-  def last_name
-  	name_arr = name.split(" ")
-  	name_arr[1..name_arr.length].join(" ")
-  end
+    def last_name
+  		name_arr = name.split(" ")
+  		name_arr[1..name_arr.length].join(" ")
+    end
 
-  # Enum's
+    # Enum's
   
-  enum year: {
+    enum year: {
 		'Kindergarten'             => 0,
 		'1st'                      => 1,
 		'2nd'                      => 2,
