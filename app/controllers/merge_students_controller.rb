6d3a9f2c4daaa3acc1ed4a34008c554f17857c72
John T Cronin <jt.cronin@outlook.com>
2017-10-06 14:10:44 -0500

    written steps that need to be taken

--- app/controllers/merge_students_controller.rb
+++ app/controllers/merge_students_controller.rb
@@ -4,3 +4,11 @@ class MergeStudentsController < ApplicationController
 		@attend = Student.all.limit(30)
 	end
 end
+
+What I need to do...
+	Have two drop down menus. Select one from each. 
+	First drop down is the one you want to merge(and delete)
+	Second drop down is the one you want the information saved to.
+
+	Take the "attendance.count" from the first and add it to the "attendance.count" for the second.
+	Delete the first choice.
