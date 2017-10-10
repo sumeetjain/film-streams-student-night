2ecb93be494f74e0f560f563d560fac9f32bdd54
John T Cronin <jt.cronin@outlook.com>
2017-10-10 11:36:33 -0500

    both students are in the view page with all of their informtation

--- app/controllers/merge_students_controller.rb
+++ app/controllers/merge_students_controller.rb
@@ -2,6 +2,17 @@ class MergeStudentsController < ApplicationController
 
 	def index
 		@attend = Student.all.limit(30)
+
+		if (params[:studentMerge] != nil)
+			redirect_to "/merge_students/show?change=#{params[:studentMerge][:firstStudent]}&keep=#{params[:studentMerge][:secondStudent]}"
+		end
+	end
+
+	def show
+		@keep = params[:keep]
+		@change = params[:change]
+		@student1 = Student.find(params[:change])
+		@student2 = Student.find(params[:keep])
 	end
 end
 
