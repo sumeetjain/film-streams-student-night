050b3612721bc2eab7cd7e0adfa5609183b763e7
Kamin Day <kaminday@gmail.com>
2017-10-10 17:07:44 -0500

    contains working psql queries

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -18,6 +18,17 @@ class StatisticsController < ApplicationController
 		@zipcodes = Student.joins(:attendances).select('students.zip').where("attendances.event_id = #{params[:id].to_i}").group(:zip).count
 		@years = Student.joins(:attendances).select('students.year').where("attendances.event_id = #{params[:id].to_i}").group(:year).count.transform_keys { |k| Student.years.key(k) }
     	# TODO: Working here to replace @referrals value with new query
+  		# 		
+#   		-- Given an event, what are the most popular referral sources?
+# -- First, just show me most popular referral sources.
+# SELECT COUNT(id) AS total, referral_type FROM referrals GROUP BY referral_type ORDER BY total DESC;
+
+# --All attendances for a given event (99)
+# SELECT * FROM attendances WHERE event_id = 99;
+
+# -- Add a column 'referral_type' to this table result.
+
+	SELECT COUNT(*) AS total, referral_type FROM attendances JOIN referrals ON attendances.student_id = referrals.student_id WHERE attendances.event_id = 98 GROUP BY referral_type ORDER BY total DESC;
     @referrals = Student.joins(:attendances).select('students.referral').where("attendances.event_id = #{params[:id].to_i}").group(:referral).count.transform_keys { |k| Student.referrals.key(k) }
 		@schools = Student.joins(:attendances, :school).where("attendances.event_id = #{params[:id].to_i}").group("schools.name").count
 		@movies = Movie.joins(:attendances).select('movies.title').where("attendances.event_id = #{params[:id].to_i}").group(:title).count
