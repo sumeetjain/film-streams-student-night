949a395229c1bbf4b5b44effb7043c216fec440b
John T Cronin <jt.cronin@outlook.com>
2017-10-10 15:54:07 -0500

    I think it destroys the old students too...

--- app/controllers/merge_students_controller.rb
+++ app/controllers/merge_students_controller.rb
@@ -21,30 +21,11 @@ class MergeStudentsController < ApplicationController
 		@student2.attendances = @student2.attendances.append(@student1.attendances)
 	end
 
-	def update
-
+	def destroy
 		@student = Student.find(params[:id])
-
-		new_values = {
-			name:               params[:student][:name],
-			year: 				params[:student][:year],
-			zip:  				params[:student][:zip],
-			email:              params[:student][:email],
-			email_confirmation: params[:student][:email_confirmation],
-			school_id:          params[:student][:school_id]
-		}
-
-		@student.update_attributes!(new_values)
-
-		flash[:notice] = "Students Merged!"
-		redirect_to "/merge_students"
+		@student.delete
+		flash[:alert] = "Student deleted."
+	    redirect_to '/merge_students'
 	end
+
 end
-
-# What I need to do...
-# 	Have two drop down menus. Select one from each. 
-# 	First drop down is the one you want to merge(and delete)
-# 	Second drop down is the one you want the information saved to.
-
-# 	Take the "attendance.count" from the first and add it to the "attendance.count" for the second.
-# 	Delete the first choice.
