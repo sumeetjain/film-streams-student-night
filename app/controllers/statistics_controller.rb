4945d67a5b3c0f9fee86fe2dce7d0675ac0e2223
Kamin Day <kaminday@gmail.com>
2017-10-11 11:21:29 -0500

    renamed testref to referrals in statistic index controller

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -8,7 +8,7 @@ class StatisticsController < ApplicationController
 		@years = Event.all.map(&:date).map(&:year).uniq
 		@students = Attendance.all.group_by_year(:created_at).select(:student_id).uniq.count
 		@unique_schools = Attendance.group_by_year('attendances.created_at').joins(:student => :school).select('schools.name').uniq.count
-		@testref = Referral.select('referrals.id').group('referrals.referral_type').count.transform_keys { |k| Referral.referral_types.key(k) }
+		@referrals = Referral.select('referrals.id').group('referrals.referral_type').count.transform_keys { |k| Referral.referral_types.key(k) }
 
 	end
   
