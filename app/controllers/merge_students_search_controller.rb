9a94b1134e59ce3326ad1168d0a2f4298251c579
John T Cronin <jt.cronin@outlook.com>
2017-10-12 14:08:53 -0500

    ready to break things down

--- app/controllers/merge_students_search_controller.rb
+++ app/controllers/merge_students_search_controller.rb
@@ -1,5 +1,13 @@
 class MergeStudentsSearchController < ApplicationController
 	def index
+		if (params[:searchChange] != nil)
+			redirect_to "/merge_students/show?change=#{params[:searchChange][:name]}"
+		end
 	end
-	
+
+	def edit
+		@change = params[:change].to_s
+		@student1 = Student.find_student(@change)
+	end
+
 end
