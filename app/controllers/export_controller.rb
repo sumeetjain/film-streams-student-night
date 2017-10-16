b066febeddba7ce07df9e5452c203282865365cb
John T Cronin <jt.cronin@outlook.com>
2017-10-16 14:07:34 -0500

    testing gem

--- app/controllers/export_controller.rb
+++ app/controllers/export_controller.rb
@@ -1,4 +1,9 @@
 class ExportController < ApplicationController
 	def index
+		@students = Student.order('name ASC')
+	  	respond_to do |format|
+	    	format.html
+	    	format.xlsx
+		end
 	end
 end
