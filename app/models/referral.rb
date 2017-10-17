9bdda2df6073703bc5a78573a1cd5242b3e745fe
Kamin Day <kaminday@gmail.com>
2017-10-17 15:13:25 -0500

    removed previous versions enum documentation from referral model

--- app/models/referral.rb
+++ app/models/referral.rb
@@ -1,18 +1,6 @@
 class Referral < ActiveRecord::Base
 	belongs_to :student
-	# Previous Versions Enums
 
-	 #  enum referral: {
-		# 	'From a friend'             => 0,
-		# 	'From my teacher'           => 1,
-		# 	"Film Stream's enewsletter" => 2,
-		# 	'Internet Search'			=> 3,
-		# 	'Social media'              => 4,
-		# 	"Film Stream's website"     => 5,
-		# 	'other source'				=> 999
-
-		# }
-	
 	# Rails enumeration for referral types
 	enum referral_type: {
 		'From a friend'             => 0,
