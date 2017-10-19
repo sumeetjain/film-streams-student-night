5bf2d41a5305025b68a81d8e62bb26aabf90541a
Kamin Day <kaminday@gmail.com>
2017-10-19 13:21:56 -0500

    added a function to get the number of unique students in attendance by location grouped by year

--- app/helpers/statistics_helper.rb
+++ app/helpers/statistics_helper.rb
@@ -65,6 +65,16 @@ module StatisticsHelper
     return attends_by_year
   end
 
+  # Gets the number of unique students per location grouped by year
+  def get_students_per_location(location_id)
+    students_by_year = @conn.exec("SELECT COUNT(DISTINCT student_id) as students, extract(year from date) as year FROM attendances JOIN events ON attendances.event_id = events.id WHERE events.location = 0 GROUP BY year;")
+    unique_student_count = {}
+    students_by_year.each do |unique_students|
+      unique_student_count[unique_students['year']] = unique_students['total']
+    end
+    return unique_student_count
+  end
+
   def print_referral_types(referrals)
     student_referrals = []
     referrals.each do |referral|
