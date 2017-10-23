01b3686614385bd4c3a403e5a6df093e99ac9dac
Kamin Day <kaminday@gmail.com>
2017-10-23 12:13:38 -0500

    updated location students to group by year'

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -11,14 +11,16 @@ class StatisticsController < ApplicationController
 		@students = Attendance.all.group_by_year(:created_at).select(:student_id).uniq.count
 		@unique_schools = Attendance.group_by_year('attendances.created_at').joins(:student => :school).select('schools.name').uniq.count
 		@referrals = Referral.select('referrals.id').group('referrals.referral_type').count.transform_keys { |k| Referral.referral_types.key(k) }
+		
 	# mine
 		@years_for_ruth_sokolof = Event.where(location: 0 ).map(&:date).map(&:year).uniq
 		@years_for_the_dundee = Event.where(location: 1 ).map(&:date).map(&:year).uniq
 		@events_by_location = events_per_location
 		# total attendaces/students per loc
 		@location_attends = Event.where(location: 0).joins(:attendances).count
-	    @location_students = Event.where(location: 0).joins(:attendances).select('attendances.student_id').uniq.count
+	    @location_students = Event.where(location: 0).joins(:attendances).group_by_year('attendances.created_at').select('attendances.student_id').uniq.count
 	    @location_schools = Event.where(location: 0).joins(:attendances => :student).select('students.school_id').uniq.count
+		binding.pry	
 
 		somestuff = ''
 	end
